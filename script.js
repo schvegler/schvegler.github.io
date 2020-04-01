4d582c99fce0bceedc269b5b58e7442dcee01960
Tamas Schvegler <tamas.schvegler@gmail.com>
2020-04-01 21:49:35 +0200

    Site finished - remake responsive design-- no privacy policy yet

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+
 var closePanel;
 var downloadPanel;
 var detailPanel;
 var anim;
 var showDlIcon;
 var img;
+var pp;
 var video;
-
+var zomiLogo;
 var photosBaby = new Array();
 var photosSnake = new Array();
 var photosBox = new Array();
@@ -13,6 +15,9 @@ var photoGroup = [photosBaby, photosSnake, photosBox];
 var videos = [];
 var titles = ["Baby Chicks", "Classis Snake", "Hit The Box"];
 var descriptions = ["Children deserve fun games too, this is why baby chick was born. Controlling the chicken mom by tilting the device is fun just like car racing games, this combined with the 2d platformer aspect gives you a new style of gameplay. Collect all the baby chicks and bring them back to your nest.", "Classic Snake, the well known retro snake game with a new aspect, beautiful graphics, cool particle effects but the same gameplay mechanism.", "Proud to anounce, Hit the box 2.0 is released and available to iOS and Android for free. This was a major update, changed the whole graphic assets and sounds. Try it out now!", "The biggest project of Zomigames, an online top down shooter for iOS and Android. This game combines battle royale with and top down shooter with some RPG elements to give you a complex experience that newer gets boring. Still in early stage of development."];
+
+
+
 
 var titleDetail;
 var textDetail;
@@ -25,20 +30,29 @@ document.onreadystatechange = function () {
       closePanel = document.getElementById("close-panel");
       downloadPanel = document.getElementById("download-panel") 
       detailPanel = document.getElementById("detail-panel");
+      document.getElementById("privacy-link").addEventListener("click", OpenPrivacy);
       document.getElementById("download").addEventListener("click", OpenDownload);
       document.getElementById("close-detail").addEventListener("click", CloseDetail);
       closePanel.addEventListener("click", CloseDownload);
       document.getElementById("baby").addEventListener("click", BabyDetail);
       document.getElementById("snake").addEventListener("click", SnakeDetail);
       document.getElementById("box").addEventListener("click", BoxDetail);
+      zomiLogo = document.getElementById("logo");
       titleDetail = document.getElementById("title");
       textDetail = document.getElementById("text");
       video = document.getElementById("video"); 
       GetBabyPhotos();
       GetSnakePhotos();
       GetBoxPhotos();
+      
   }
 }
+
+function OpenPrivacy() {
+    window.open("pp"+"html", '_blank');
+    
+}
+
 
 function GetBabyPhotos() {
     for (i = 0; i < 5; i++) {
@@ -58,21 +72,34 @@ function GetBoxPhotos() {
     }
 }
 
+ function scrollToTop() { 
+     var timerHandle = setInterval(function() {
+         if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) 
+             window.scrollBy(0,-30); 
+         else clearInterval(timerHandle); },10); 
+ }
+
 
 function BabyDetail() {
+    scrollToTop();
+    document.getElementById("download").style.opacity = 0;
     detailPanel.style.top = 0; 
     titleDetail.textContent = titles[0];
     textDetail.textContent = descriptions[0];
-    video.src = "babyvideo.m4v";
+    pp = "baby";
+    video.src = "babyvideo.mp4";
     for (i=0; i < photosBox.length; i++) {
         img[i].src = photosBaby[i];
     }
 }
 
 function SnakeDetail() {
+    scrollToTop();
+    document.getElementById("download").style.opacity = 0;
     detailPanel.style.top = 0; 
     titleDetail.textContent = titles[1];
     textDetail.textContent = descriptions[1];
+    pp = "snake";
     video.src = "snakevideo.mp4";
     for (i=0; i < photosSnake.length; i++) {
         img[i].src = photosSnake[i];
@@ -80,9 +107,12 @@ function SnakeDetail() {
 }
 
 function BoxDetail() {
+    scrollToTop();
+    document.getElementById("download").style.opacity = 0;
     detailPanel.style.top = 0; 
     titleDetail.textContent = titles[2];
     textDetail.textContent = descriptions[2];
+    pp = "box";
     video.src = "boxvideo.mp4";
     for (i=0; i < photosBox.length; i++) {
         img[i].src = photosBox[i];
@@ -90,17 +120,24 @@ function BoxDetail() {
 }
 
 function CloseDetail() {
-    detailPanel.style.top = "-1500px";
+    document.getElementById("download").style.opacity = 1;
+    detailPanel.style.top = "-300%";
 }
 
 
 function OpenDownload() {
-    downloadPanel.style.width = "640px";
+    document.getElementById("download").style.transition = "0.5s";
+    document.getElementById("download").style.opacity = 0;
+    closePanel.style.transition = "0.4s";
+    closePanel.style.transform = "rotate(720deg)";
+    downloadPanel.style.width = "100%"
     closePanel.style.opacity = 1;
     showDlIcon = setTimeout(ShowDownloadIcons, 500);
 }
 
 function CloseDownload() {
+    document.getElementById("download").style.opacity = 1;
+    closePanel.style.transform = "rotate(45deg)";
     downloadPanel.style.width = "0px";
     closePanel.style.opacity = 0;
     HideDownloadIcons();
