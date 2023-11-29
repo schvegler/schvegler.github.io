0cd399bd6252ac443db65be646f0e43f9f39e5f3
Tamas Schvegler <tamas.schvegler@gmail.com>
2023-11-29 06:26:11 +0100

    Lots of changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var photosBox = new Array();
 var photoGroup = [photosBaby, photosSnake, photosBox];
 var videos = [];
 var titles = ["Feathery Fury", "Classis Snake", "Hit The Box"];
-var descriptions = ["Children deserve fun games too, this is why Feathery Fury was born. Controlling the chicken mom by tilting the device is fun just like car racing games, this combined with the 2d platformer aspect gives you a new style of gameplay. Collect all the baby chicks and bring them back to your nest.", "Classic Snake, the well known retro snake game with a new aspect, beautiful graphics, cool particle effects but the same gameplay mechanism.", "Proud to anounce, Hit the box 2.0 is released and available on iOS for free. This was a major update, changed the whole graphic assets and sounds. Try it out now!", "The biggest project of Zomigames, an online top down shooter for iOS and Android. This game combines battle royale with and top down shooter with some RPG elements to give you a complex experience that newer gets boring. Still in early stage of development."];
+var descriptions = ["Fun games for children are a must, which is why Feathery Fury was created. Tilt your device to guide the chicken mom, blending excitement with classic 2D platformer gameplay. Your mission: collect all the adorable baby chicks and lead them back to the safety of your nest.", "Introducing Classic Snake: the beloved retro game revamped with stunning visuals, captivating effects, yet retaining its iconic gameplay mechanics.", "Woah, big news! Hit the Box 2.0 just dropped on iOS, and it's completely revamped with new graphics and fresh sounds. And the best part? It's free! Give it a whirl now and dive into the upgraded experience!", "The biggest project of Zomigames, an online top down shooter for iOS and Android. This game combines battle royale with and top down shooter with some RPG elements to give you a complex experience that newer gets boring. Still in early stage of development."];
 
 var titleDetail;
 var textDetail;
@@ -23,7 +23,7 @@ var textDetail;
 
 document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loading');
-    setTimeout(function() {loadNow(1);}, 3000);
+    setTimeout(function() {loadNow(1);}, 1000);
 
     anim = document.getElementsByClassName("anim");
     img = document.getElementsByClassName("img");
